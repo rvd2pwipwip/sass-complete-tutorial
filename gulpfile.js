284bc8c8bed9ceda6bbea5adbeab9c20d309ae14
Herve Desrosiers <hdesrosiers@stingray.com>
2021-12-22 09:50:27 -0500

    20-purging css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-const { src, dest, watch, series } = require('gulp')
-const sass = require('gulp-sass')(require('sass'));
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src('shinobi/**/*.scss')
+  return src("shinobi/**/*.scss")
     .pipe(sass())
-    .pipe(dest('css'))
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(['shinobi/**/*.scss'], buildStyles)
+  watch(["shinobi/**/*.scss", "*.html"], buildStyles);
 }
 
-exports.default = series(buildStyles, watchTask)
+exports.default = series(buildStyles, watchTask);
